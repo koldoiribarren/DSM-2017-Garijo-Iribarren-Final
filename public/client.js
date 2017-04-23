08ddc81fb016d3dffc51a0ccfec64137b3b6f066
Luis Garijo <luisgarijog@gmail.com>
2017-04-23 18:54:00 +0200

    Error en client.js o

--- public/client.js
+++ public/client.js
@@ -1,3 +1,13 @@
+// Establish the connection with the socket
+var socket = io.connect('http://localhost:8080');
+
+// We assign a new 'on' event to the socket using the name of the new communication, 
+// and define the callback function that acts when the information arrives.
+socket.on('login', function(data){
+
+});
+
+
 $(document).ready(function() {
 
 	$('#pass_bool').on('change', function(){
@@ -13,9 +23,17 @@ $(document).ready(function() {
 		var form = $(this);
 		var formData = form.serialize();
 		alert(formData);
-		$.ajax({type: 'POST', url: '/middle1', data: formData}).done(function(data){
+		$.ajax({type: 'POST', url: '/login', data: formData}).done(function(data){
 			console.log(data);
 			form.trigger('reset');
+
+
+			 if (datos[0] == true || datos[1] == false){
+
+			 } else if(datos[0] == false || datos[1] == true){
+
+			 };
+
 		});
 	});
 	$('#register_form').on('submit', function(event) {
@@ -23,10 +41,12 @@ $(document).ready(function() {
 		var form = $(this);
 		var formData = form.serialize();
 		alert(formData);
-		$.ajax({type: 'POST', url: '/middle2', data: formData}).done(function(data){
+		$.ajax({type: 'POST', url: '/register', data: formData}).done(function(data){
 			console.log(data);
 			form.trigger('reset');
 		});
 	});
 
 });
+
+
---
+++ server.js
@@ -0,0 +1,114 @@
+var express = require('express');
+var app = express();
+
+var mongoose = require('mongoose');
+
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+
+mongoose.Promise = require('bluebird');
+var connection_options = {server : {auto_reconnect: true}, user: 'admin', pass: 'castellano100'};
+db = mongoose.connect('ds149820.mlab.com', 'search_engine_3', 49820, connection_options, function(error){
+	if (!error){
+		console.log('Connecting to database');
+	} else {
+		console.log(' Error connecting ' + error);
+	}
+});
+
+
+app.use(express.static('public'));
+
+
+
+app.post('/login', parseUrlencoded, function(request, response) {
+
+	// var received = request.body;
+	// console.log('User ' + received.user + ' successfully logged in');
+	// response.json('Login data received');
+	var datos = request.body;
+	
+	console.log('___________________LOG IN_____________________________');
+	console.log("Nombre de usuario recibido: " + datos.user);
+
+
+	//BUSCAMOS TODOS LOS USUARIOS
+	Usuario.find().count(function(error_find_count, count) {
+
+		// FORMA DE LOS DATOS DE VUELTA AL CLIENTE
+		//  [UsuarioAlmacenado, nicknameOcupado] -> puede ser true o false
+		if (error_find_count) {
+						// error en la búsqueda
+			response.json([false, false]);
+			console.log('Error al buscar todos los usuarios:' + error_find_count);
+			console.log('______________________________________________________');
+		} else {
+				// si hay más de 10 usuarios conectados
+			if (count >= MaxNumberOfUsers) {
+				console.log('Hay 10 usuarios conectados, límite superado');
+				response.json([false, false]);
+
+			} else {
+				// No hay 10 usuarios conectados vamos a comprobar que el nickname no esté ocupado
+				Usuario.find({nickname: datos.user}, function(error_find,docs) {
+				console.log('Hay menos de 10 usuarios conectados');
+
+					if (error_find) {
+						//error en la búsqueda
+						response.json([false, false]);
+						console.log('Error al buscar el nickname del usuario:' + error_find);
+						console.log('______________________________________________________');
+					} else if (docs.length == 0) {
+
+						// Nickname libre, almacenamos el usuario
+						console.log('Nickname libre, almacenando usuario...');
+						var insert_user = new Usuario({nickname: datos.user});
+						insert_user.save(function(error) {
+						
+							if (!error) {
+								//almacenamiento correcto
+								response.json([true, false]);
+								console.log('...usuario almacenado');
+								console.log('______________________________________________________');
+							} else {
+								//error al almacenar
+								response.json([false, false]);
+								console.log('Error al almacenar el usuario:' + error);
+								console.log('______________________________________________________');
+							}
+						});
+						
+					} else {
+
+						// Existe el usuario
+						response.json([false, true]);
+						console.log('Nickname ocupado');
+						console.log('______________________________________________________');
+					}
+				});
+
+			}
+		}
+	});
+
+});
+
+app.post('/register', parseUrlencoded, function(request, response) {
+	var received = request.body;
+	
+	console.log(received.user + ' + ' + received.pass);
+	response.json('Register data received');
+});
+
+
+server.listen(8080, function(){
+	console.log('Listening on port 8080');
+});
+
+io.on('login', function(client) {
+
+});
